41168dfdfa37b5651981f64a8d1a30ba10bab3bb
Sare <83638067+hollowlhjo@users.noreply.github.com>
2023-06-08 18:28:45 +0300

    com

--- script.js
+++ script.js
@@ -1,87 +1,143 @@
-class Snake {
-  constructor(mas) {
-    this.head = 0;
-    this.archive = [this.head];
-    this.segments = [];
-    this.moving = "right";
-    this.mayMove = ["right", "left", "down", "up"];
-    }
-    renderNewInt(int){
-      if(int > -1){
-        let oldInt = 0
-        oldInt = int
-        console.log(oldInt)
-        return oldInt
-      }
-    }
-    move(event) {
-      let newPlace = 0
-      let moving = "right"
-      if (event.keyCode === 39 && moving != "left") {
-        newPlace += 1 
-        moving = "right"
-        cellSnaked.renderNewInt(newPlace)
-      } else if (event.keyCode === 37 && moving != "right") {
-        newPlace -= 1
-        moving = "left"
-        cellSnaked.renderNewInt(newPlace)
-      } else if (event.keyCode === 40 && moving != "up") {
-        newPlace += 10
-        moving = "down"
-        cellSnaked.renderNewInt(newPlace)
-      } else if (event.keyCode === 38 && moving != "down") {
-        newPlace -= 10
-        moving = "up"
-        cellSnaked.renderNewInt(newPlace)
-      }
-  }
+const cells = document.querySelectorAll(".cell");
+let btn = document.getElementById("btn");
+let moveInt;
+let snake = {
+  snakeAlive:1,
+  points:1,
+  length:1,
+  masCoordinates:[],
+  coordinates:0,
+  moving:"right",
+  speed: 250
 }
-function createfield(cells, indexes){
-  cells.forEach((cell, index) => {
-    cell.classList.add("game-cell");
-    if (index === 0) {
-      cell.classList.add("snake");
-      let cellSnaked = cell;
-      let indexSnaked = index;
-    }
-    indexes.push(index);
+function death(){
+  snake.snakeAlive = 0;
+  alert("Game over");
+  clearInterval(moveInt);
+  btn.disabled = false
+  snake = {
+    snakeAlive:1,
+    points:1,
+    length:1,
+    masCoordinates:[],
+    coordinates:0,
+    moving:"right",
+    speed: prompt("Enter speed(standart:250) when 1000 is slower")
+  }
+  clearInterval(timer)
+  document.getElementById("points").innerHTML = "Points: " + snake.points;
+  cells.forEach(cell => {
+    cell.classList.remove("snake");
+    cell.classList.remove("food");
+    cell.classList.remove("snakeTail");
   });
 }
-class Game {
-  constructor(lenghtSnake, level) {
-    this.amountFree = 100 - lenghtSnake;
+function init() {
+  let sec = 0;
+  let timer = setInterval(tick, 1000);
+}
+
+function tick() {
+  sec++;
+  document.getElementById('timer').innerHTML = "Timer:  " + sec;
+}
+
+function direction(event) {
+  if (event.keyCode === 39 && snake.moving !== "left") {
+    snake.moving = "right";
+  } else if (event.keyCode === 37 && snake.moving !== "right") {
+    snake.moving = "left";
+  } else if (event.keyCode === 40 && snake.moving !== "up") {
+    snake.moving = "down";
+  } else if (event.keyCode === 38 && snake.moving !== "down") {
+    snake.moving = "up";
   }
 }
-class Food {
-  constructor(cells, lenghtSnake) {
-    cells.forEach((elem) => {
-      if (elem.classList.contains("snake")) {
-      } else {
-        let cell = Math.random() * 100;
-        if (cell > 97) {
-          elem.classList.add("food");
+
+function move() {
+  if (snake.moving == "left") {
+    if (snake.coordinates % 10 === 0) {
+      death()
+    } else {
+      snake.coordinates -= 1;
+    }
+  }
+  if (snake.moving == "down") {
+    snake.coordinates += 10;
+    if (snake.coordinates > 100) {
+      death()
+    }
+  }
+  if (snake.moving == "up") {
+    snake.coordinates -= 10;
+    if (snake.coordinates < 0) {
+      death()
+    }
+  }
+  if (snake.moving == "right") {
+    if ((snake.coordinates + 1) % 10 === 0) {
+      death()
+    } else {
+      snake.coordinates += 1;
+    }
+  }
+  createField(cells);
+}
+
+function createField() {
+  cells.forEach((cell, index) => {
+    cell.classList.add("game-cell");
+    if (index === snake.coordinates) {
+      cell.classList.add("snake");
+      snake.masCoordinates.unshift(cell);
+      if (snake.points > 0 && snake.length < snake.points) {
+        snake.length += 1;
+        if (snake.moving === "right") {
+          cells[index - 1].classList.add("snake");
+        } else if (snake.moving === "left") {
+          cells[index + 1].classList.add("snake");
+        } else if (snake.moving === "up") {
+          cells[index + 10].classList.add("snake");
+        } else if (snake.moving === "down") {
+          cells[index - 10].classList.add("snake");
         }
+      }
+      if (cell.classList.contains("food")) {
+        snake.points += 1;
+        cell.classList.remove("food");
+        document.getElementById("points").innerHTML = "Points: " + snake.points;
+        let food = new Food(cells);
+      }
+    }
+  });
+
+  if (snake.masCoordinates.length > snake.length) {
+    snake.masCoordinates.pop().classList.remove("snake");
+  }
+
+  if (snake.masCoordinates.some((c, i) => i !== 0 && c === snake.masCoordinates[0])) {
+    death()
+  }
+}
+
+class Food {
+  constructor(cells) {
+    let cell = Math.floor(Math.random() * 100);
+    if (cells[cell].classList.contains("snake") || cells[cell].classList.contains("snakeTail")) {
+      cell = Math.floor(Math.random() * 100);
+    }
+    cells.forEach((elem, index) => {
+      if (!elem.classList.contains("snake") && index === cell) {
+        elem.classList.add("food");
       }
     });
   }
 }
+
 function InitGame() {
-  let snakeAlive = 1;
-  const cells = document.querySelectorAll(".cell");
-  let indexes = [];
-  createfield(cells, indexes)
-  let cells1 = Array.prototype.slice.call(cells);
-  cellSnaked = new Snake(cells1);
-  newPlace = cellSnaked.renderNewInt(-1)
-  console.log(newPlace)
-  cells.forEach((cell, index) => {
-    if (index === newPlace) {
-      cell.classList.add("snake");
-      var cellSnaked = cell;
-    }
-  });
-  let food = new Food(cells, 1);
-  document.addEventListener("keydown", cellSnaked.move)
-  setInterval(console.log(newPlace), 1000);
+  btn.disabled = true;
+  init();
+  document.addEventListener("keydown", direction);
+  let food = new Food(cells);
+  moveInt = setInterval(move, snake.speed);
 }
-InitGame()
--- timer.js
+++
@@ -1,9 +0,0 @@
-function init() {
-    sec = 0;
-    setInterval(tick, 1000);
-}
-
-function tick() {
-    sec++;
-    document.getElementById('timer').innerHTML = "Timer:  " + sec;
-}
